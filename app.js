23615df3a2944087223913478a406f70da720594
abhay-04y <abhay0423yadav@gmail.com>
2025-08-27 22:13:56 +0530

    Leacture-02

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag in child1."),
-    React.createElement("h2", {}, "I am h2 tag in child1."),
+    React.createElement(
+      "h1",
+      { id: "inner1-child1" },
+      "I am h1 tag in child1."
+    ),
+    React.createElement(
+      "h2",
+      { id: "inner1-child2" },
+      "I am h2 tag in child1."
+    ),
   ]),
   [
     React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I am h1 tag in child2."),
-      React.createElement("h2", {}, "I am h2 tag in child2."),
+      React.createElement(
+        "h1",
+        { id: "inner2-child1" },
+        "I am h1 tag in child2."
+      ),
+      React.createElement(
+        "h2",
+        { id: "inner2-child2" },
+        "I am h2 tag in child2."
+      ),
     ]),
   ],
 ]);
